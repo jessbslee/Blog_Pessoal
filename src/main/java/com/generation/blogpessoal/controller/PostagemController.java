ae20d421b17f4d550b579fae6c5e9edc436e27c0
jessbslee <jess.b.silva@gmail.com>
2022-05-04 11:32:36 -0300

    Atualização PostagemController

--- src/main/java/com/generation/blogpessoal/controller/PostagemController.java
+++ src/main/java/com/generation/blogpessoal/controller/PostagemController.java
@@ -19,13 +19,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.generation.blogpessoal.model.Postagem;
 import com.generation.blogpessoal.repository.PostagemRepository;
+import com.generation.blogpessoal.repository.TemaRepository;
 
 	@RestController
 	@RequestMapping ("/postagens")
 	@CrossOrigin (origins = "*", allowedHeaders = "*")
 	public class PostagemController {
+		
 		@Autowired
 		private PostagemRepository postagemRepository;
+		
+		@Autowired
+		private TemaRepository temaRepository;
 		
 		@GetMapping
 			public ResponseEntity <List<Postagem>> getAll(){
@@ -46,19 +51,35 @@ import com.generation.blogpessoal.repository.PostagemRepository;
 		
 		@PostMapping
 		public ResponseEntity <Postagem> postPostagem(@Valid @RequestBody Postagem postagem){
-		return ResponseEntity.status(HttpStatus.CREATED).body(postagemRepository.save(postagem));  
+			
+			if (temaRepository.existsById(postagem.getTema().getId()))		
+		return ResponseEntity.status(HttpStatus.CREATED).body(postagemRepository.save(postagem)); 
+			
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();  
 		}
 		
 		@PutMapping
 		public ResponseEntity <Postagem> putPostagem(@Valid @RequestBody Postagem postagem){
-		return ResponseEntity.status(HttpStatus.OK).body(postagemRepository.save(postagem));  
+			
+			if (postagemRepository.existsById(postagem.getId())){
+				
+				if (temaRepository.existsById(postagem.getTema().getId()))
+		return ResponseEntity.status(HttpStatus.OK).body(postagemRepository.save(postagem));
+				
+				return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();     
+			}
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();    
 		}
 		
 		@DeleteMapping("/{id}")
-		public void deletePostagem(@PathVariable Long id){
-		postagemRepository.deleteById(id);	
-		
-		//Deleta a postagem
+		public ResponseEntity<?> deletePostagem(@PathVariable Long id) {
+			
+			return postagemRepository.findById(id)
+					.map(resposta -> {
+						postagemRepository.deleteById(id);
+						return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+					})
+					.orElse(ResponseEntity.notFound().build());  
 		}
 		
 }
