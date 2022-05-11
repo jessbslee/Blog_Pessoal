4dd3119de32eef1174fdb365a1f5fe60045cc4f8
jessbslee <jess.b.silva@gmail.com>
2022-05-10 21:23:45 -0300

    Add Spring Security

--- src/main/java/com/generation/blogpessoal/model/Postagem.java
+++ src/main/java/com/generation/blogpessoal/model/Postagem.java
@@ -38,6 +38,18 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 	@ManyToOne
 	@JsonIgnoreProperties("postagem")
 	private Tema tema;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Usuario usuario;
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
 
 	public Long getId() {
 		return id;
